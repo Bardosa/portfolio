072673e6fcfdb67c213a335c5f420256d2f0d0a4
Lucas <artistcbarbosa@gmail.com>
2022-09-21 15:39:52 -0300

    atualizar

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ function showMenu() {
 toggle.addEventListener('click', showMenu)
 
 linksMenu.forEach(e => e.addEventListener('click', showMenu))
+
+
+//====== Sobre
+
+const bodyWidth = document.getElementById('body').clientWidth
+let sobreTexto = document.getElementById('sobre_text')
+
+if(bodyWidth <= 684){
+    sobreTexto.innerHTML = `Minha busca por transformação e crescimento pessoal me trouçe até aqui, sou apaixonado por programação por não gostar da repetição monotona do dia a dia. <a href="https://www.linkedin.com/in/lucas-barbosa-9951bb242" target="_blank" class="link">Ver mais...</a><br><br>Eu sou Lucas Barbosa, desenvolvedor front-end, mas logo serei mais.`
+}
